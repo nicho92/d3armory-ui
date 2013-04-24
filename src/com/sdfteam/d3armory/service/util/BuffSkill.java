e41f318a1b29d9980bd910ae7ca8ae8f775c4ec6
nicolas.pihen@gmail.com <nicolas.pihen@gmail.com@685c5a13-573f-31e0-148c-40973e854c4f>
2013-04-24 07:37:43 +0000

    Start with Buff Skills

--- src/com/sdfteam/d3armory/service/util/BuffSkill.java
+++ src/com/sdfteam/d3armory/service/util/BuffSkill.java
@@ -3,21 +3,29 @@ package com.sdfteam.d3armory.service.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.armory.d3.beans.Item;
 import org.armory.d3.beans.MinMaxBonus;
 
 public class BuffSkill {
 
 	private static Map<String,MinMaxBonus> buffs; 
 		
-	public static Map<String,MinMaxBonus> getBuff(EnumerationBuffs b) {
+	public static Map<String,MinMaxBonus> getBuff(EnumerationBuffs b,Map<EnumerationStuff,Item> stuffs) {
 		
 		buffs = new HashMap<String, MinMaxBonus>();
 		switch (b) {
-		case FOCUSEDMIND : buffs.put("Attacks_Per_Second_Item_Bonus_"+b, new MinMaxBonus(0.3));
+		case FOCUSEDMIND : buffs.put("Attacks_Per_Second_Item_Bonus_"+b, new MinMaxBonus(0.03));
 			break;
-		case ANATOMY:
+		case ANATOMY: buffs.put("Crit_Percent_Bonus"+b, new MinMaxBonus(0.03));
 			break;
-		case ARCHERY:
+		case ARCHERY: {
+				if(stuffs.get(EnumerationStuff.MAIN_HAND).getType().getId().equals("Crossbow"))
+					buffs.put("Crit_Damage_"+b, new MinMaxBonus(0.5));
+				if(stuffs.get(EnumerationStuff.MAIN_HAND).getType().getId().equals("HandXbow"))
+					buffs.put("Crit_Percent_"+b, new MinMaxBonus(0.1));
+				if(stuffs.get(EnumerationStuff.MAIN_HAND).getType().getId().equals("Bow"))
+					buffs.put("Damage_Weapon_Percent_Bonus#Physical_"+b, new MinMaxBonus(0.03));
+			}
 			break;
 		case BATTLERAGE:
 			break;
