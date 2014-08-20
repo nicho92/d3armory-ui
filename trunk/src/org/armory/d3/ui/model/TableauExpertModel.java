83f2bd9849d6dcecd144c80b1c50779134a84172
nicolas.pihen@gmail.com <nicolas.pihen@gmail.com@685c5a13-573f-31e0-148c-40973e854c4f>
2014-08-20 15:23:30 +0000

    add column for Expert table

--- trunk/src/org/armory/d3/ui/model/TableauExpertModel.java
+++ trunk/src/org/armory/d3/ui/model/TableauExpertModel.java
@@ -7,15 +7,19 @@ import javax.swing.table.DefaultTableModel;
 import org.armory.d3.services.D3ArmoryControler;
 
 import com.pihen.d3restapi.beans.MinMaxBonus;
+import com.pihen.d3restapi.service.util.BuffCalculator;
 
 public class TableauExpertModel extends DefaultTableModel {
 
 	public String getColumnName(int c) {
 		if(c==0)
 			return "Bonus";
-		else
+		if(c==1)
+			return "Item/Skill";
+		if(c==2)
 			return "Valeur";
 		
+		return "";
 	}
 
 	
@@ -25,7 +29,7 @@ public class TableauExpertModel extends DefaultTableModel {
 
 
 	public int getColumnCount() {
-		return 2;
+		return 3;
 	}
 
 
@@ -41,8 +45,11 @@ public class TableauExpertModel extends DefaultTableModel {
 	 public Class getColumnClass(int colonne) {
 	     if (colonne==0) {
            return String.class;
-         } 
-         if (colonne==1) {
+         }
+	     if (colonne==1) {
+	           return String.class;
+	         } 
+         if (colonne==2) {
              return Double.class;
           } 
          return Object.class; 
@@ -55,11 +62,21 @@ public class TableauExpertModel extends DefaultTableModel {
 		Map<String,MinMaxBonus> map = D3ArmoryControler.getInstance().getCalculator().getStatAttributs();
 		Object[] entries= map.entrySet().toArray();
 	        Map.Entry<String, MinMaxBonus> entry=(Map.Entry)entries[row];
+	        int pos = entry.getKey().lastIndexOf("_");
+	        
 	        if (column==0) {
-	            return entry.getKey();
-	        } else if (column==1) { // column==1
+	        	String ret = entry.getKey().replaceAll(BuffCalculator.PREFIX, "_").substring(0, pos);
+	
+	        	return ret;
+	        } 
+	        else if (column==1) { // column==1
+	        	return entry.getKey().substring(pos+1, entry.getKey().length());
+	        } 
+	        else if (column==2) { // column==1
 	            return entry.getValue().getMoyenne();
-	        } else {
+	        } 
+	        
+	        else {
 	            throw new IndexOutOfBoundsException("MapTableModel provides a 2-column table, column-index "+column+" is illegal.");
 	        } 
         
